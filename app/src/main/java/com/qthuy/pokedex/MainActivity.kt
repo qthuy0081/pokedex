5a851f2c63ac2a0b152eb9cef8af5e61b7b07a7c
Quốc Thụy Đinh <qthuy.0081@gmail.com>
2021-11-09 22:46:23 +0700

    replace some error dependencies

--- app/src/main/java/com/qthuy/pokedex/MainActivity.kt
+++ app/src/main/java/com/qthuy/pokedex/MainActivity.kt
@@ -2,10 +2,18 @@ package com.qthuy.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.coroutines.runBlocking
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.textView)
+        runBlocking {
+
+        }
     }
+
 }
